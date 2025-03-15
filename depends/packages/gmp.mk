2f0d965bc1cd32c89366e9569ad292b328c01884
CodeIsTheKey <codeiskey@protonmail.com>
2025-03-15 11:55:49 -0600

    gmp cxxflags update

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -6,6 +6,7 @@ $(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a
 $(package)_patches=applem1.patch
 
 define $(package)_set_vars
+$(package)_cxxflags+=-std=c++17
 $(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
 $(package)_cflags_armv7l_linux+=-march=armv7-a
 $(package)_config_opts_arm_darwin+=--build=$(subst arm,aarch64,$(BUILD)) --host=$(subst arm,aarch64,$(HOST))
